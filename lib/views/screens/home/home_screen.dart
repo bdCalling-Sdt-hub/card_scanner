3ebbb846b6a07b584c624da843572e17fe7ffc71
naimulhassan2001 <developernaimul00@gmail.com>
2024-03-11 15:52:29 +0600

    added app strings

--- lib/views/screens/home/home_screen.dart
+++ lib/views/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,6 +8,31 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
+
+      // body:  Center(
+      //   child: DottedBorder(
+      //     padding: EdgeInsets.all(0),
+      //     dashPattern: [10,6],
+      //     color: Colors.green,
+      //     strokeWidth: 1,
+      //     borderType: BorderType.RRect,
+      //     radius: Radius.circular(12),
+      //     child: Container(
+      //       decoration: BoxDecoration(
+      //         color: Colors.amberAccent,
+      //         borderRadius: BorderRadius.circular(12)
+      //       ),
+      //       width: 200,
+      //       height: 200,
+      //       child: Center(
+      //         child: Text(
+      //           'Dotted Border',
+      //           style: TextStyle(fontSize: 20),
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
