3aac4d936ec3bdcc9028265f4a06d29e67c27089
Siamjht <siambci@gmail.com>
2024-03-14 17:26:55 +0600

    some works of auth section done

--- lib/views/screens/Auth/forget_password_screen.dart
+++ lib/views/screens/Auth/forget_password_screen.dart
@@ -2,7 +2,9 @@
 import 'package:card_scanner/controllers/auth/forgotPasswordController.dart';
 import 'package:card_scanner/views/widgets/customText/custom_text.dart';
 import 'package:card_scanner/views/widgets/custom_text_field/custom_text_field.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -26,25 +28,57 @@ class ForgotPasswordScreen extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SizedBox(height: 60.h, width: Get.width),
                   SizedBox(
                     height: 20.h,
                   ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: GestureDetector(
+                      onTap: () {
+                        Get.back();
+                      },
+                      child: Container(
+                        height: 32.h,
+                        width: 32.w,
+                        decoration: BoxDecoration(
+                          color: AppColors.black_400,
+                          borderRadius: BorderRadius.circular(4)
+                        ),
+                        child: const Icon(Icons.arrow_back_ios_rounded, color: AppColors.whiteColor, size: 14,),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 54.h, width: Get.width),
                   CustomText(
-                    text: AppStrings.signUpNOw,
+                    text: AppStrings.forgotPassword,
                     fontWeight: FontWeight.w600,
-                    fontSize: 32,
+                    fontSize: 28,
+                    color: AppColors.black_500,
                   ),
                   SizedBox(
                     height: 8.h,
                   ),
-                  CustomText(
-                    text: AppStrings.fillTheDetails,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                    color: AppColors.black_400,
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.h),
+                    child: CustomText(
+                      maxLines: 2,
+                      text: AppStrings.enterYourEmail,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                      color: AppColors.black_400,
+                    ),
                   ),
-
+                  SizedBox(height: 24.h,),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: CustomText(
+                      text: AppStrings.email,
+                      color: AppColors.black_500,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 8.h,),
 
                   ///<<<====================Email Field=================================>>>
 
@@ -82,27 +116,55 @@ class ForgotPasswordScreen extends StatelessWidget {
                     height: 24.h,
                   ),
 
-                  ///<<<================= Contact Number field==============================>>>
-                  CustomText(
-
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        color: AppColors.black_900,
+                        width: 135.w,
+                        height: 1.h,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12.w),
+                        child: CustomText(
+                          text: AppStrings.or,
+                          color: AppColors.black_500,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Container(
+                        color: AppColors.black_900,
+                        width: 135.w,
+                        height: 1.h,
+                      )
+                    ],
                   ),
+
+                  ///<<<================= Contact Number field==============================>>>
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: CustomText(
+                      text: AppStrings.contactNumber,
+                      color: AppColors.black_500,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 8.h,),
+
                   CustomTextField(
                     // textEditingController: forgotPasswordController.passwordController,
                     validator: (value) {
                       if (value.isEmpty) {
                         return AppStrings.fieldCantBeEmpty.tr;
-                      } else if (value.length < 8) {
-                        return AppStrings.passMustContainBoth.tr;
-                      } else if (!AppStrings.passRegExp.hasMatch(value)) {
-                        return AppStrings.passMustContainBoth.tr;
                       } else {
                         return null;
                       }
                     },
-                    isPassword: true,
                     keyboardType: TextInputType.text,
                     textAlign: TextAlign.start,
-                    hintText: AppStrings.newPassword.tr,
+                    hintText: AppStrings.enterMobileNumber.tr,
                     hintStyle: GoogleFonts.prompt(
                         fontSize: 16.h,
                         fontWeight: FontWeight.w400,
@@ -112,8 +174,6 @@ class ForgotPasswordScreen extends StatelessWidget {
                         fontSize: 16.h,
                         color: AppColors.black_500),
                     fieldBorderRadius: 8,
-                    isPrefixIcon: false,
-                    suffixIconColor: AppColors.black_400,
                     // prefixIcon: Icon(
                     //   Icons.lock_outlined,
                     //   size: 24.h,
@@ -126,22 +186,27 @@ class ForgotPasswordScreen extends StatelessWidget {
 
                   SizedBox(height: 8.h),
                   SizedBox(height: 54.h,),
-                  CustomElevatedButton(
-                    height: 42.h,
-                    width: 158.w,
-                    isFillColor: false,
-                    onTap: () {},
-                    borderRadius: 12,
-                    borderColor: AppColors.black_500,
-                    text: AppStrings.getOtp,
-                    textColor: AppColors.black_500,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  )
                 ],
               ),
             ),
           ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 24.h),
+        child: CustomElevatedButton(
+          height: 50.h,
+          width: Get.width,
+          isFillColor: true,
+          onTap: () {
+
+          },
+          borderRadius: 12,
+          backgroundColor: AppColors.black_500,
+          text: AppStrings.getOtp,
+          textColor: AppColors.whiteColor,
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
     );
   }
 }
